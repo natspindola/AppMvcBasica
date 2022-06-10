25f1a911db2616009f5a34a180215c22c7260835
Perk <natspindola@hotmail.com>
2022-06-10 19:16:41 -0300

    mapeando os campos

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,23 @@ namespace AppMvcBasica.Models
     {
         public Guid FornecedorId { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres ", MinimumLength = 2)]
         public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Valor { get; set; }
+
         public DateTime DataCadastro { get; set; }
+
         public string Ativo { get; set; }
 
         /* EF Relation */
